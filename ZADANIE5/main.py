c241553771900a38a18f24066585822e8d3fa0ce
lukasz1_malec <lmalec493@gmail.com>
2025-01-23 09:04:33 +0100

    zadanie 5

--- ZADANIE5/main.py
+++ ZADANIE5/main.py
@@ -15,25 +15,37 @@ def create_app():
     3) Przycisk "Pokaż", który wyświetli wpisany tekst w innej etykiecie
     """
     # title - "Prosta aplikacja Tkinter"
+    root = tk.Tk()
+    root.title("Prosta aplikacja Tkinter")
 
     # label_instruct = umocuj przez pack
+    label_instruct = tk.Label(root, text="Wpisz coś:")
+    label_instruct.pack()
 
-    # entry_text = 
+    # entry_text =
+    entry_text = tk.Entry(root)
+    entry_text.pack() 
 
     # label_result = tk.Label(...
+    label_result = tk.Label(root, text="")
+    label_result.pack()
 
     # zdefiniuj funkcję show_text() pobierającą wpisany tekst i wyświetlającą w label_result
-    # def show_text():
-    #     ...
-    #     label_result.config(...)
+    def show_text():
+        entered_text = entry_text.get()
+        label_result.config(text=f"Wpisałeś: {entered_text}")
 
     # button_show = 
     # button_show.pack()
 
-    # TODO: return root
-    pass
+    button_show = tk.Button(root, text="Pokaż", command=show_text)
+    button_show.pack()
+
+    return root
 
 if __name__ == '__main__':
     # TODO: app = create_app()
     # TODO: app.mainloop()
-    pass
+    app = create_app()
+    app.mainloop()
+
